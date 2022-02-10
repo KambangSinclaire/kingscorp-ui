e874700575e8474b98f62ef887ed1b7394abd93e
Kambang Sinclaire <kambangsinclaire@gmail.com>
2022-02-10 07:52:06 +0100

    updated deployment configs

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback');
 const path = require("path");
 
 const port = process.env.PORT || 7878;
 const app = express()
+app.use(history());
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get('/.*/', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-app.listen(port, () => console.log("Served frontend on Port ", port));
+app.listen(port, () => console.log("Kingscorp frontend running on Port ", port));
