a8905b634db9a65833eed2716822f1297751d9f2
Kambang Sinclaire <kambangsinclaire@gmail.com>
2022-02-18 01:45:02 +0100

    fixed authorization and x-api-key issue

--- src/interceptors/request.interceptor.ts
+++ src/interceptors/request.interceptor.ts
@@ -2,39 +2,32 @@
 import store from '@/store'
 import { getFromStorage } from '@/utils/storage.util'
 import axios from 'axios'
-const userCredentials = getFromStorage('user')
 
-console.log(userCredentials);
 
 const requestInterceptor = async () => {
-try {
-    let isUser = store.getters.getUsers
+    try {
+        return axios.interceptors.request.use((config) => {
+            let userCredentials = getFromStorage('user')
+            if (!config.url?.includes('ipapi.co/json')) {
+                config.headers = {
+                    'Content-Type': "application/json",
+                    'Authorization': userCredentials ? `Bearer ${userCredentials.refresh_token}` : "",
+                    "x-api-key": userCredentials ? userCredentials.x_api_key : "",
+                }
+                if (config.url?.includes('app')) {
+                    store.dispatch('showLoader', "spinner")
+                }
 
-    if(!isUser) {
-        isUser = userCredentials
+                store.dispatch('showLoader', "skeleton")
+            }
+
+            return config
+        })
+
+    } catch (error) {
+        store.dispatch('showLoader')
+        Promise.reject(error)
     }
-
-   return axios.interceptors.request.use((config) => {
-       if(!config.url?.includes('ipapi.co/json')){
-           config.headers = {
-               'Content-Type':"application/json",
-               'Authorization':!userCredentials?'':"Bearer "+userCredentials['refresh_token'] ?? "",
-               "x-api-key":!userCredentials?'':userCredentials['x_api_key'],
-           }
-           if(config.url?.includes('app')){
-               store.dispatch('showLoader', "spinner")
-           }
-           
-           store.dispatch('showLoader', "skeleton")
-       }
-        
-        return config
-    })
-    
-} catch (error) {
-    store.dispatch('showLoader')
-    Promise.reject(error)
-}
 }
 
 export default requestInterceptor
