92f87cb71d35045e16cbf11ba29a449ac29c6387
Uchenna David <uched0643@gmail.com>
2022-03-26 12:18:07 +0000

    fixed profile ui

---
+++ src/utils/pagination.ts
@@ -0,0 +1,12 @@
+
+interface paginatedData {
+    fields:[]
+}
+const paginateDataFunction = (paginated:object | string , index:number, paginatedData:any[], memo = {}) => {
+    try {
+        let data:any;
+        //  paginatedData.filter()
+    } catch (error) {
+        console.log(error);
+    }
+}
